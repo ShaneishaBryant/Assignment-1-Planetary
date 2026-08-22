ea12516e6a5802f0b7a17f9013e60f9f1db76c41
ShaneishaBryant <shaneisharichardson@student.bpcc.edu>
2026-08-22 16:49:51 -0500

    Phase 2 Complete
    I followed these steps to complete phase 2:
    > created a variable to ask the temperature for the visiting planet
    > used if-else statements to set up conditions

--- ProgrammingProblem/src/Journal.md
+++ ProgrammingProblem/src/Journal.md
@@ -1,5 +1,11 @@
 How did you handle the input for the user’s name and the planet’s name? Did you encounter any issues with the Scanner?
 
 
-For the user's and plant name, I created separate prompts to store the user's response. I used the 'sout' method to print out the response. 
+For the user's and plant name, I created separate variables to store the user's response. I used the 'sout' method to print out the response. 
 I didn't encounter any issues with the scanner. When I began typing 'scanner' the system auto imported the scanner package.  
+
+------
+
+Explain the logic you used to determine if a planet is “Hazardous.” Why did you choose that specific conditional structure?
+
+I was experiencing issues using nextInt() to get the temperature at first. However, after reading a helpful article that recommended using nextLine() with the Scanner variable, I resolved the issue. I then used an if-else statement to set my specific conditions. I also used the || (logical OR) operator to combine both conditions (temp > 100 || temp < -50), which helped keep my code short.
--- ProgrammingProblem/src/Main.java
+++ ProgrammingProblem/src/Main.java
@@ -3,17 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner greeting = new Scanner(System.in);
+        Scanner program = new Scanner(System.in);
 
         System.out.println("Welcome to The Galatic Bureau of Interstellar Tourism\n" +
                 "proudly guiding curious minds across the cosmos. \nEnter your name:");
-        String name = greeting.nextLine();
+        String name = program.nextLine();
 
         System.out.println("Enter the name of the plant you wish to visit:");
-        String plant = greeting.nextLine();
+        String plant = program.nextLine();
 
 
         System.out.println("Greetings," + name + " you wish to visit " + plant + ".");
 
+
+        System.out.println("Let's check the plants temperature.\n" +
+                "What is the planet's average temperature in degrees Celsius?");
+        int temp = program.nextInt();
+
+        if (temp > 100 || temp < -50) {
+            System.out.println("Caution: This plant is too extreme for human life.");
+        } else {
+            System.out.println("This planet's temperature is safe to visit.");
+
+        }
     }
 }
